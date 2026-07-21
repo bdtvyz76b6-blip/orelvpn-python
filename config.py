cb243b10bb48063ff9fb7df12b2b6b8b81214dcd
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-21 09:06:40 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+BOT_TOKEN = "ТВОЙ_ТОКЕН"
+
+WIFI_LINK = "https://raw.githubusercontent.com/bdtvyz76b6-blip/vpn-sub/main/sub.txt"
+
+BS_LINK = "https://raw.githubusercontent.com/bdtvyz76b6-blip/vpn-sub/main/LTE.txt"
+
+FREE_TARIFF = "Wi-Fi"
+
+PAID_TARIFF = "Обход Б/С"
+
+SUPPORT = "@rusrodyyya"
+
+
+ADMIN_ID = 6312016802
+
+
+# Оплата
+CARD_NUMBER = "ТВОЯ_КАРТА"
+CARD_OWNER = "ТВОЁ_ИМЯ"
+PRICE_BS = "99 ₽"
