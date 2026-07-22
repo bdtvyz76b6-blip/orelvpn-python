08516e3c9b4238154abf6bdd57775141039dfec9
bdtvyz76b6-blip <bdtvyz76b6@privaterelay.appleid.com>
2026-07-22 12:21:37 +0300

    Update database.py

--- database.py
+++ database.py
@@ -2,8 +2,10 @@ import sqlite3
 
 DB = "users.db"
 
+
 def connect():
     return sqlite3.connect(DB)
+
 
 # Создание таблицы
 def create_table():
@@ -14,16 +16,24 @@ def create_table():
     CREATE TABLE IF NOT EXISTS users (
         user_id INTEGER PRIMARY KEY,
         username TEXT,
-        tariff TEXT DEFAULT 'Wi-Fi',
-        link TEXT DEFAULT ''
+
+        wifi_active INTEGER DEFAULT 1,
+        bs_active INTEGER DEFAULT 0,
+
+        wifi_link TEXT DEFAULT '',
+        bs_link TEXT DEFAULT '',
+
+        payment_status TEXT DEFAULT 'none'
     )
     """)
 
     conn.commit()
     conn.close()
 
+
 # Добавить пользователя
 def add_user(user_id, username):
+
     conn = connect()
     cur = conn.cursor()
 
@@ -39,64 +49,91 @@ def add_user(user_id, username):
     conn.commit()
     conn.close()
 
-# Выдать тариф
-def set_tariff(user_id, tariff, link):
+
+# Выдать Обход Б/С
+def activate_bs(user_id, link):
+
     conn = connect()
     cur = conn.cursor()
 
     cur.execute(
         """
         UPDATE users
-        SET tariff=?, link=?
+
+        SET
+        bs_active=1,
+        bs_link=?,
+        payment_status='approved'
+
         WHERE user_id=?
         """,
-        (tariff, link, user_id)
+        (link, user_id)
     )
 
     conn.commit()
     conn.close()
 
-# Получить пользователя
-def get_user(user_id):
+
+# Забрать Обход Б/С
+def remove_bs(user_id):
+
     conn = connect()
     cur = conn.cursor()
 
     cur.execute(
-        "SELECT * FROM users WHERE user_id=?",
+        """
+        UPDATE users
+
+        SET
+        bs_active=0,
+        bs_link=''
+
+        WHERE user_id=?
+        """,
+        (user_id,)
+    )
+
+    conn.commit()
+    conn.close()
+
+
+# Получить пользователя
+def get_user(user_id):
+
+    conn = connect()
+    cur = conn.cursor()
+
+    cur.execute(
+        """
+        SELECT * FROM users
+        WHERE user_id=?
+        """,
         (user_id,)
     )
 
     user = cur.fetchone()
 
     conn.close()
+
     return user
 
-# Получить всех пользователей
+
+
+# Все пользователи для админки
 def get_all_users():
-    conn = connect()
-    cur = conn.cursor()
 
-    cur.execute("SELECT * FROM users")
-
-    users = cur.fetchall()
-
-    conn.close()
-    return users
-
-# Забрать Обход Б/С
-def remove_bs(user_id):
     conn = connect()
     cur = conn.cursor()
 
     cur.execute(
         """
-        UPDATE users
-        SET tariff='Wi-Fi',
-            link=''
-        WHERE user_id=?
-        """,
-        (user_id,)
+        SELECT * FROM users
+        ORDER BY user_id DESC
+        """
     )
 
-    conn.commit()
+    users = cur.fetchall()
+
     conn.close()
+
+    return users
